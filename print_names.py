deb7c4d0f837aea40811c59b41c0aa525bdf9049
hmcostaa <hmcosta2002@gmail.com>
2023-10-16 19:30:48 +0100

    commit henrique

--- print_names.py
+++ print_names.py
@@ -4,9 +4,13 @@ def initial_print():
 def print_goncalo():
 	print('Goncalo')
 
+def print_henrique():
+    print("Henrique")
+
 def main():
 	initial_print()
 	print_goncalo()
+    print_henrique()
 
 if __name__ == '__main__':
         main()
