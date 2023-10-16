037a8e0582208111f0fb3ec062e2df81be63875e
Bárbara Reis Silva <barbarakingsilva@gmail.com>
2023-10-16 19:32:27 +0100

    Added print for Barbara

--- print_names.py
+++ print_names.py
@@ -4,9 +4,13 @@ def initial_print():
 def print_goncalo():
 	print('Goncalo')
 
+def print_barbara():
+	print('Barbara')
+
 def main():
 	initial_print()
 	print_goncalo()
+	print_barbara()
 
 if __name__ == '__main__':
         main()
