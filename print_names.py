0d90dd99ab5d494b8e5656ddd0d203cfdbe7391f
joaocosta21 <joaocostared@gmail.com>
2023-10-16 19:30:23 +0100

    joao miguel costa

--- print_names.py
+++ print_names.py
@@ -1,12 +1,19 @@
 def initial_print():
-	print('This is a name printer')
+    print('This is a name printer')
+
 
 def print_goncalo():
-	print('Goncalo')
+    print('Goncalo')
+
+
+def prin_joao():
+    print('Joao')
+
 
 def main():
-	initial_print()
-	print_goncalo()
+    initial_print()
+    print_goncalo()
+
 
 if __name__ == '__main__':
-        main()
+    main()
