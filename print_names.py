b93b96ca624fa5a3dadeefc62292ccceef39d6a7
Diogo <diogo.peralta.goncalves@gmail.com>
2023-10-16 20:03:03 +0100

    pls

--- print_names.py
+++ print_names.py
@@ -9,29 +9,41 @@ def print_goncalo():
 def print_joaoC():
     print('JoaoC')
 
+
 def print_barbara():
-	print('Barbara')
- 
+    print('Barbara')
+
+
 def print_henriqueM():
     print("Henrique M")
+
 
 def print_davidG():
     print("David Gaspar")
 
+
 def print_joao():
-	print("Joao F")
-      
-def print_goncalo():
-	print('Afonso Mestre')
+    print("Joao F")
+
+
+def print_afonso():
+    print('Afonso Mestre')
+
+
+def print_diogo():
+    print('Diogo Gonçalves')
+
 
 def main():
-	initial_print()
-	print_goncalo()
-	print_barbara()
-	print_henriqueM()
-	print_davidG()
-	print_joao()
-	print_joaoC()
+    initial_print()
+    print_goncalo()
+    print_barbara()
+    print_henriqueM()
+    print_davidG()
+    print_joao()
+    print_joaoC()
+    print_diogo()
+
 
 if __name__ == '__main__':
     main()
