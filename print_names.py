84ff7ac3ee91caa1e7d46db8dbeeea834a83153e
Quiomachado <quiomachado@gmail.com>
2023-10-16 19:26:09 +0100

    Henrique M

--- print_names.py
+++ print_names.py
@@ -4,9 +4,13 @@ def initial_print():
 def print_goncalo():
 	print('Goncalo')
 
+def print_henriqueM():
+    print("Henrique M")
+
 def main():
 	initial_print()
 	print_goncalo()
+    print_henriqueM()
 
 if __name__ == '__main__':
         main()
