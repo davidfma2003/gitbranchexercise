13c637a022b0e2c93894227202410a497aade0cd
Noronha01 <pedronoronha.g@gmail.com>
2023-10-16 19:45:42 +0100

    Pedro printing function

--- print_names.py
+++ print_names.py
@@ -10,11 +10,19 @@ def print_henriqueM():
 def print_joao():
 	print("Joao F")
 
+def print_pedro():
+        my_name = ""
+        letters = ["P", "e", "d", "r", "o"]
+        for letter in letters:
+                        my_name += letter
+        print(my_name)
+
 def main():
 	initial_print()
 	print_goncalo()
     print_henriqueM()
 	print_joao()
+	print_pedro()
 
 if __name__ == '__main__':
         main()
