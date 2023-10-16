707eda0f943d92a983eabf788353f3bda0904c29
Gonçalo Frazão <gonfrazao@gmail.com>
2023-10-16 18:26:13 +0100

    feat: print goncalo

--- print_names.py
+++ print_names.py
@@ -1,8 +1,12 @@
 def initial_print():
 	print('This is a name printer')
 
+def print_goncalo():
+	print('Goncalo')
+
 def main():
-	initial_print() 
+	initial_print()
+	print_goncalo()
 
 if __name__ == '__main__':
         main()
