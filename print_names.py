e50deb276a074e35ef982cc427d68af0e5db5129
davidmarafuzgaspar <djm.gaspar@gmail.com>
2023-10-16 19:41:40 +0100

    lezz go!

--- print_names.py
+++ print_names.py
@@ -7,10 +7,14 @@ def print_goncalo():
 def print_henriqueM():
     print("Henrique M")
 
+def print_davidG():
+    print("David Gaspar")
+
 def main():
 	initial_print()
 	print_goncalo()
     print_henriqueM()
+ 	print_davidG()
 
 if __name__ == '__main__':
         main()
